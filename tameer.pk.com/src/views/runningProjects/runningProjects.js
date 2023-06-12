7c72ed831a09dab047d56b38576ff9e46eed6ebe
arslanrajpot <arslanarshad2307@gmail.com>
2023-06-13 02:56:43 +0500

    view running Projects added

---
+++ tameer.pk.com/src/views/runningProjects/runningProjects.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Projects from 'src/components/Project'
+
+const runningProjects = () => {
+  return <Projects status={'ACTIVE'} />
+}
+export default runningProjects
